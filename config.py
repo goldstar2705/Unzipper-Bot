1beeec26a300e9043c502f8ffe4899499e7dd7d2
goldstar2705 <65126516+goldstar2705@users.noreply.github.com>
2022-01-02 20:46:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = int(os.environ.get("1286763"))
+    API_HASH = os.environ.get("676238949450c128a465ec4c0cc9c170")
+    BOT_TOKEN = os.environ.get("729621416:AAF2Y7bnqtFUUnZOYaOZ9zUHV52y95HFhFk")
+    LOGS_CHANNEL = int(os.environ.get("-1001648589085"))
+    MONGODB_URL = os.environ.get("mongodb+srv://Goldstar:qwerty27052003@cluster0.osssn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    BOT_OWNER = int(os.environ.get("669114984"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
     TG_MAX_SIZE = 2040108421
